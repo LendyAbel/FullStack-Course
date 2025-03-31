f19607a9ead2340c9f41010166150e6600db866b
LendyAbel <lendyabel93@gmail.com>
2025-03-31 20:36:02 +0200

    phonebook Step11 Fixed last merge

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState, useEffect } from "react"
-import contacts from "./services/contacts"
+import { useState, useEffect } from 'react'
+import contacts from './services/contacts'
 
-import Filter from "./components/Filter"
-import AddContactForm from "./components/AddContactForm"
-import ShowNumbers from "./components/ShowNumbers"
-import Notification from "./components/Notification"
-
-const isNumberRepeated = (newPerson, olderPerson) =>
-  newPerson.number === olderPerson.number
+import Filter from './components/Filter'
+import AddContactForm from './components/AddContactForm'
+import ShowNumbers from './components/ShowNumbers'
+import Notification from './components/Notification.jsx'
 
 const isNumberRepeated = (newPerson, olderPerson) =>
   newPerson.number === olderPerson.number
@@ -15,19 +12,18 @@ const isNumberRepeated = (newPerson, olderPerson) =>
 const App = () => {
   // console.log('-------------------------')
   const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState("")
-  const [newNumber, setNewNumber] = useState("")
-  const [filterText, setFilterText] = useState("")
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filterText, setFilterText] = useState('')
   const [message, setMessage] = useState(null)
 
   useEffect(() => {
-    contacts.getAllContacts().then((contacts) => {
+    contacts.getAllContacts().then(contacts => {
       setPersons(contacts)
     })
   }, [])
 
   const resetFrom = () => {
-<<<<<<< HEAD
     setNewName('')
     setNewNumber('')
   }
@@ -35,39 +31,16 @@ const App = () => {
     contacts.replaceContact(newPerson).then(updatePerson => {
       setPersons(persons.map(p => (p.id != updatePerson.id ? p : updatePerson)))
       resetFrom()
-    })
-  }
-  const addNewContact = newPerson => {
-    contacts.addContact(newPerson).then(newPerson => {
-      setPersons(persons.concat(newPerson))
-      resetFrom()
-    })
-  }
-
-  const add = e => {
-    e.preventDefault()
-    if (!newName || !newNumber) return alert('Name or Number is empty')
-
-    let newPerson = { name: newName, number: newNumber }
-    const existingPerson = persons.find(person => person.name === newName)
-=======
-    setNewName("")
-    setNewNumber("")
-  }
-  const replaceContact = (newPerson) => {
-    contacts.replaceContact(newPerson).then((updatePerson) => {
-      setPersons(
-        persons.map((p) => (p.id != updatePerson.id ? p : updatePerson))
+      setMessage(
+        `${updatePerson.name} number changed to ${updatePerson.number}`
       )
-      resetFrom()
-      setMessage(`${updatePerson.name} number changed to ${updatePerson.number}`)
       setTimeout(() => {
         setMessage(null)
       }, 5000)
     })
   }
-  const addNewContact = (newPerson) => {
-    contacts.addContact(newPerson).then((newPerson) => {
+  const addNewContact = newPerson => {
+    contacts.addContact(newPerson).then(newPerson => {
       setPersons(persons.concat(newPerson))
       resetFrom()
       setMessage(`Added ${newPerson.name}`)
@@ -77,13 +50,12 @@ const App = () => {
     })
   }
 
-  const add = (e) => {
+  const add = e => {
     e.preventDefault()
-    if (!newName || !newNumber) return alert("Name or Number is empty")
+    if (!newName || !newNumber) return alert('Name or Number is empty')
 
     let newPerson = { name: newName, number: newNumber }
-    const existingPerson = persons.find((person) => person.name === newName)
->>>>>>> 1cdf4157441d576427fef2160e5cd96f28c4274c
+    const existingPerson = persons.find(person => person.name === newName)
 
     if (existingPerson) {
       newPerson.id = existingPerson.id
@@ -102,34 +74,29 @@ const App = () => {
     addNewContact(newPerson)
   }
 
-<<<<<<< HEAD
   const deleteContact = e => {
     if (e.target.tagName !== 'BUTTON') return
-=======
-  const deleteContact = (e) => {
-    if (e.target.tagName !== "BUTTON") return
->>>>>>> 1cdf4157441d576427fef2160e5cd96f28c4274c
 
     const deletedContactId = e.target.id
-    const personToDelete = persons.find((person) => person.id === e.target.id)
+    const personToDelete = persons.find(person => person.id === e.target.id)
     // console.log(e.target.id)
     // console.log(personToDelete)
 
     if (window.confirm(`Delete ${personToDelete.name}?`)) {
       contacts.deleteContact(deletedContactId).then(() => {
-        setPersons(persons.filter((person) => person.id != deletedContactId))
+        setPersons(persons.filter(person => person.id != deletedContactId))
       })
     }
   }
 
   //OnChange Handlers
-  const nameOnChange = (e) => {
+  const nameOnChange = e => {
     setNewName(e.target.value)
   }
-  const numberOnChange = (e) => {
+  const numberOnChange = e => {
     setNewNumber(e.target.value)
   }
-  const filterOnChange = (e) => {
+  const filterOnChange = e => {
     setFilterText(e.target.value)
   }
 
