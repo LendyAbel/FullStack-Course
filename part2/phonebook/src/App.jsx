9c3c5c9f6b3ad790ed81f286508a509e310f0ae5
LendyAbel <lendyabel93@gmail.com>
2025-03-31 14:33:21 +0200

    phonebook Step11 Created and added Notification component

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react'
-import contacts from './services/contacts'
+import { useState, useEffect } from "react"
+import contacts from "./services/contacts"
 
-import Filter from './components/Filter'
-import AddContactForm from './components/AddContactForm'
-import ShowNumbers from './components/ShowNumbers'
+import Filter from "./components/Filter"
+import AddContactForm from "./components/AddContactForm"
+import ShowNumbers from "./components/ShowNumbers"
+import Notification from "./components/Notification"
 
 const isNumberRepeated = (newPerson, olderPerson) =>
   newPerson.number === olderPerson.number
@@ -11,39 +12,50 @@ const isNumberRepeated = (newPerson, olderPerson) =>
 const App = () => {
   // console.log('-------------------------')
   const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filterText, setFilterText] = useState('')
+  const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
+  const [filterText, setFilterText] = useState("")
+  const [message, setMessage] = useState(null)
 
   useEffect(() => {
-    contacts.getAllContacts().then(contacts => {
+    contacts.getAllContacts().then((contacts) => {
       setPersons(contacts)
     })
   }, [])
 
   const resetFrom = () => {
-    setNewName('')
-    setNewNumber('')
+    setNewName("")
+    setNewNumber("")
   }
-  const replaceContact = newPerson => {
-    contacts.replaceContact(newPerson).then(updatePerson => {
-      setPersons(persons.map(p => (p.id != updatePerson.id ? p : updatePerson)))
+  const replaceContact = (newPerson) => {
+    contacts.replaceContact(newPerson).then((updatePerson) => {
+      setPersons(
+        persons.map((p) => (p.id != updatePerson.id ? p : updatePerson))
+      )
       resetFrom()
+      setMessage(`${updatePerson.name} number changed to ${updatePerson.number}`)
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
     })
   }
-  const addNewContact = newPerson => {
-    contacts.addContact(newPerson).then(newPerson => {
+  const addNewContact = (newPerson) => {
+    contacts.addContact(newPerson).then((newPerson) => {
       setPersons(persons.concat(newPerson))
       resetFrom()
+      setMessage(`Added ${newPerson.name}`)
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
     })
   }
 
-  const add = e => {
+  const add = (e) => {
     e.preventDefault()
-    if (!newName || !newNumber) return alert('Name or Number is empty')
+    if (!newName || !newNumber) return alert("Name or Number is empty")
 
     let newPerson = { name: newName, number: newNumber }
-    const existingPerson = persons.find(person => person.name === newName)
+    const existingPerson = persons.find((person) => person.name === newName)
 
     if (existingPerson) {
       newPerson.id = existingPerson.id
@@ -62,35 +74,36 @@ const App = () => {
     addNewContact(newPerson)
   }
 
-  const deleteContact = e => {
-    if (e.target.tagName !== 'BUTTON') return
+  const deleteContact = (e) => {
+    if (e.target.tagName !== "BUTTON") return
 
     const deletedContactId = e.target.id
-    const personToDelete = persons.find(person => person.id === e.target.id)
+    const personToDelete = persons.find((person) => person.id === e.target.id)
     // console.log(e.target.id)
     // console.log(personToDelete)
 
     if (window.confirm(`Delete ${personToDelete.name}?`)) {
       contacts.deleteContact(deletedContactId).then(() => {
-        setPersons(persons.filter(person => person.id != deletedContactId))
+        setPersons(persons.filter((person) => person.id != deletedContactId))
       })
     }
   }
 
   //OnChange Handlers
-  const nameOnChange = e => {
+  const nameOnChange = (e) => {
     setNewName(e.target.value)
   }
-  const numberOnChange = e => {
+  const numberOnChange = (e) => {
     setNewNumber(e.target.value)
   }
-  const filterOnChange = e => {
+  const filterOnChange = (e) => {
     setFilterText(e.target.value)
   }
 
   return (
     <div>
       <h1>Phonebook</h1>
+      <Notification message={message} />
       <Filter filterText={filterText} filterOnChange={filterOnChange} />
       <AddContactForm
         newName={newName}
