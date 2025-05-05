a51240b8e3cf3fe5728a67317ce784a1308a0bbe
LendyAbel <lendyabel93@gmail.com>
2025-05-05 09:09:42 +0200

    commit after pull

--- introdemo/src/services/notes.js
+++ introdemo/src/services/notes.js
@@ -1,9 +1,5 @@
 import axios from 'axios'
-<<<<<<< HEAD
-const baseUrl = '/api/notes'
-=======
 const baseUrl = 'http://localhost:3001/api/notes'
->>>>>>> aac46dc93815ab3202e41bc5c1927d9b5b219d26
 
 const getAll = () => {
   const request = axios.get(baseUrl)
